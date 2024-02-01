00719de8e8f46eb80fc0f600e0c1d9cf33e66ca2
Jurabek <jorabeksindarov44@gmail.com>
2024-02-01 20:41:24 +0500

    Product seed model created and ProductList razor page

---
+++ Ecommerce.Client/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,9 @@
+using Ecommerce.Client.Shared;
+
+namespace Ecommerce.Client.Client.Shared;
+
+public partial class ProductList
+{
+    private static List<Product> Products = new List<Product>();
+    
+}
