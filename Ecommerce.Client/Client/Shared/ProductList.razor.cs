3400a55f25c779943d1f7067119b8a76e11e614e
Jurabek <jorabeksindarov44@gmail.com>
2024-02-01 21:48:26 +0500

    ProductList used

--- Ecommerce.Client/Client/Shared/ProductList.razor.cs
+++ Ecommerce.Client/Client/Shared/ProductList.razor.cs
@@ -4,6 +4,24 @@ namespace Ecommerce.Client.Client.Shared;
 
 public partial class ProductList
 {
-    private static List<Product> Products = new List<Product>();
-    
+    private static List<Product> Products = new List<Product>()
+    {
+        new Product()
+        {
+            Id = 1,
+            Title = "1984",
+            Description =
+                "1984\" (inglizcha: Nineteen Eighty-Four) ingliz yozuvchisi va publitsisti George Orwellning antiutopiya janrida yozilgan romani. 1949-yil nashr etilgan. Roman 2018-yil Karim Bahriyev tomonidan oʻzbekchaga tarjima qilingan",
+            ImageUrl = "https://prod-bb-images.akamaized.net/book-covers/coverimage-9782291090830-bookwire-2021-05-07.jpg?w=640",
+            Price = 9.99m
+        },
+        new Product()
+        {
+            Id = 2,
+            Title = "Shovvoz Gatsby",
+            Description = "Shovvoz Gatsby (inglizcha: The Great Gatsby) 2013-yilda katta ekranlarga chiqqan, Buyuk Gatsby kitobiga asoslangan romantik va drama janrlaridagi AQSh filmidir. Film rejissori Baz Luhrmann boʻlib, bu kartina Warner Bros. Pictures tomonidan tarqatilgandir.",
+            ImageUrl = "https://wab.org/wp-content/uploads/2015/06/gatsbycast-350x161.jpg",
+            Price = 9.99m
+        },
+    };
 }
