351399580cf05d321e3cf19bcd89cde4fdeae20c
Jurabek <jorabeksindarov44@gmail.com>
2024-02-08 14:36:45 +0500

    Product controller added

---
+++ Ecommerce.Client/Server/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+using Ecommerce.Client.Server.Services.ProductService;
+using Ecommerce.Client.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Client.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsAsync()
+    {
+        var result = await _productService.GetProductsAsync();
+        return Ok(result);
+    }
+}
